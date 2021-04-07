23b5cc2cd67775b2659e8f023a77338097d347da
전은형 <ooooo5h91@gmail.com>
2021-04-07 21:04:57 +0900

    가게 정보를 뿌려줄 어댑터 생성 도중, 한 줄 화면 만들러 감

--- app/src/main/java/com/neppplus/self2pizzaorderapp_20210407/adapters/StoreListAdapter.kt
+++ app/src/main/java/com/neppplus/self2pizzaorderapp_20210407/adapters/StoreListAdapter.kt
@@ -1,11 +1,25 @@
 package com.neppplus.self2pizzaorderapp_20210407.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.self2pizzaorderapp_20210407.R
 import com.neppplus.self2pizzaorderapp_20210407.Store
 
 class StoreListAdapter(
     val mContext : Context,
     val resId : Int,
     val mList : ArrayList<Store>) : ArrayAdapter<Store>(mContext, resId, mList) {
+
+    val inflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if(tempRow == null) {
+//            tempRow = inflater.inflate(R.layout.)
+        }
+    }
 }
