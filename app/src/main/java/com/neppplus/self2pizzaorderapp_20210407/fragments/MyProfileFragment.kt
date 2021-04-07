43ba214a3c883ef3ce2e5469b47ba80ab326e662
전은형 <ooooo5h91@gmail.com>
2021-04-07 21:14:32 +0900

    실제 가게 정보 입력 완료

--- app/src/main/java/com/neppplus/self2pizzaorderapp_20210407/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/self2pizzaorderapp_20210407/fragments/MyProfileFragment.kt
@@ -6,8 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.self2pizzaorderapp_20210407.R
+import com.neppplus.self2pizzaorderapp_20210407.Store
 
 class MyProfileFragment : Fragment() {
+
+    val mStoreList = ArrayList<Store>()
 
 
 
@@ -21,5 +24,11 @@ class MyProfileFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        mStoreList.add( Store("피자헛", "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg", "1588-5588" ))
+        mStoreList.add( Store("파파존스", "http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800", "1577-8080" ))
+        mStoreList.add( Store("미스터피자", "https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200", "1577-0077" ))
+        mStoreList.add( Store("도미노피자", "https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png", "1577-3082" ))
+
     }
 }
