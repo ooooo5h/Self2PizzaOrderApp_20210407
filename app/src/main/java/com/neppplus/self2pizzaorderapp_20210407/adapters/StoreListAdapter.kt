62353ed8e33c26fd4d29da34657485d00d5fe5f9
전은형 <ooooo5h91@gmail.com>
2021-04-07 21:22:36 +0900

    텍스트뷰는 나오게 했지만, 로고를 만들자!!

--- app/src/main/java/com/neppplus/self2pizzaorderapp_20210407/adapters/StoreListAdapter.kt
+++ app/src/main/java/com/neppplus/self2pizzaorderapp_20210407/adapters/StoreListAdapter.kt
@@ -1,10 +1,13 @@
 package com.neppplus.self2pizzaorderapp_20210407.adapters
 
 import android.content.Context
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import com.neppplus.self2pizzaorderapp_20210407.R
 import com.neppplus.self2pizzaorderapp_20210407.Store
 
@@ -22,6 +25,14 @@ class StoreListAdapter(
             tempRow = inflater.inflate(R.layout.store_list_item, null)
         }
         val row = tempRow!!
+
+        val clickedStore = mList[position]
+
+        val logoImg = row.findViewById<ImageView>(R.id.logoImg)
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+
+
+        nameTxt.text = clickedStore.name
 
         return row
     }
